c2db286e7ed6dcb037a301a13901aa4451f5eb18
Tony <schmittaj@gmail.com>
2019-04-08 09:17:46 -0500

    Fixes

--- seam.py
+++ seam.py
@@ -17,15 +17,15 @@ def entropy_sobel(image):
 def find_vert_seam(entropyImage):
     minImg = np.zeros((entropyImage.shape[0], entropyImage.shape[1]))
     minPath = np.zeros((entropyImage.shape[0], entropyImage.shape[1]))
-    for x in range(0, entropyImage.shape[1]-1):
+    for x in range(0, entropyImage.shape[1]):
         minImg[0,x] = entropyImage[0, x, 0] + entropyImage[0, x, 1] + entropyImage[0, x, 2]
         x = x + 1
 
     # going top to bottom
-    for y in range(1, entropyImage.shape[0]-1):
-        for x in range(0,entropyImage.shape[1]-1):
+    for y in range(1, entropyImage.shape[0]):
+        for x in range(0,entropyImage.shape[1]):
             pixent = int(entropyImage[y, x, 0]) + int(entropyImage[y, x, 1]) + int(entropyImage[y, x, 2])
-            minent, minpathVal = find_min(entropyImage.shape[0],x,y,minImg)
+            minent, minpathVal = find_min(entropyImage.shape[1],x,y,minImg)
             minPath[y, x] = minpathVal
             minImg[y, x] = minent + pixent
             #print(minImg[y, x])
@@ -36,22 +36,43 @@ def find_vert_seam(entropyImage):
 
 def find_min(maxSize, curPlaceX, curPlaceY, minImg):
     values = [999999, 999999, 999999]
-    for ctr in range(-1,1):
+    for ctr in range(-1,2):
         if 0 <= ctr + curPlaceX < maxSize:
             values[ctr+1] = int(minImg[curPlaceY-1, ctr+curPlaceX])
-    # if values[0] < values[1]:
-    #     index = -1
-    #     if values[0] > values[2]:
-    #         index = 1
-    # else:
-    #     index = 0
-    #     if values[1] > values[2]:
-    #         index = 1
     return min(values), values.index(min(values)) - 1
-    #return values[index+1], index
 # end function
 
 
-image = load_img("japan.jpg")
+image = load_img("test.jpg")
+
 entropy = entropy_sobel(image)
 minVals, minPath = find_vert_seam(entropy)
+#cv.imshow("",entropy)
+#cv.waitKey(0)
+
+
+f = open("output.txt", "w")
+
+f.write("Values:" + "\n")
+for y in range(0,minPath.shape[0]):
+    line = ""
+    for x in range(0,minPath.shape[1]):
+        showVal = int(entropy[y, x, 0]) + int(entropy[y, x, 1]) + int(entropy[y, x, 2])
+        line += "" + str(showVal) + " "
+    f.write(line + "\n")
+
+f.write("\n" + "MinVals:" + "\n")
+for y in range(0,minPath.shape[0]):
+    line = ""
+    for x in range(0,minPath.shape[1]):
+        line += "" + str(minVals[y, x]) + " "
+    f.write(line + "\n")
+
+f.write("\n" + "MinPath:" + "\n")
+for y in range(0,minPath.shape[0]):
+    line = ""
+    for x in range(0,minPath.shape[1]):
+        line += "" + str(minPath[y, x]) + " "
+    f.write(line + "\n")
+
+f.close()
